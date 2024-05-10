fc0cb8b4e727f391eac48e9ec6973e0cc9c16512
Kyuseishu <kyuseishu1593@gmail.com>
2024-05-10 19:02:11 +0700

    refactor: handle indexedDB

--- src/utils/shares/db/answer/models/Test.ts
+++ src/utils/shares/db/answer/models/Test.ts
@@ -1,14 +1,14 @@
 import IMiniTest from "@utils/shares/interfaces/IMiniTest"
 import ITest from "@utils/shares/interfaces/ITest"
 
-import { db } from '../index'
+import { db } from '../../index'
 
 class TestModel {
     constructor() {
     }
 
     getTable() {
-        return db.tests
+        return db.answerExams
     }
 }
 
