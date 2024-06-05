1233cc0527907c328b323e38f136fe2ea3134dfe
Kyuseishu <kyuseishu1593@gmail.com>
2024-06-05 17:54:51 +0700

    fix: logo

--- src/app/(client)/_components/header.tsx
+++ src/app/(client)/_components/header.tsx
@@ -22,16 +22,16 @@ export default function Header() {
                         <section className="hidden sm:flex items-center justify-between py-2">
                             <div className="logo lg:w-28 sm:w-28 rounded-full shadow-xl">
                                 <Link href="/" className="logo">
-                                    <img
+                                    {/* <img
                                         alt=""
                                         className="w-100 w-full"
                                         src={process.env.NEXT_PUBLIC_APP_LOGO}
-                                    />
-                                    {/* <Image
+                                    /> */}
+                                    <Image
                                         alt=""
                                         className="w-100 w-full"
                                         src={process.env.NEXT_PUBLIC_APP_LOGO || ''}
-                                    /> */}
+                                    />
                                 </Link>
                             </div>
                             {/* banner */}
@@ -46,16 +46,16 @@ export default function Header() {
                         <section className="header-mobile flex sm:hidden justify-between items-center px-2 py-2 shadow shadow-lg-[#000]">
                             <section className="logo mobile w-16">
                                 <Link href="/" className="logo">
-                                    <img
+                                    {/* <img
                                         alt=""
                                         className="w-100 w-full"
                                         src={process.env.NEXT_PUBLIC_APP_LOGO}
-                                    />
-                                    {/* <Image
+                                    /> */}
+                                    <Image
                                         alt=""
                                         className="w-100 w-full"
                                         src={process.env.NEXT_PUBLIC_APP_LOGO || ''}
-                                    /> */}
+                                    />
                                 </Link>
                             </section>
                             <nav className="h-full flex md:hidden items-center">
